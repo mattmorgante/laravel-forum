9a511007e042aff02dafb5a6e542cbc86f9958c9
Matt Morgante <matthewmorgante@gmail.com>
2017-05-16 16:39:39 +0200

    profile page

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 
+Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
